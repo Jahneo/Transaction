c7dd47d5f91e617e15bb283dd43be0e06a225dad
Jahneo <neil4hall@gmail.com>
2021-11-16 02:01:20 -0500

    edit server.js

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(MONGODB_URI, {
+  useUnifiedTopology: true, 
   useFindAndModify: false,
   useNewUrlParser: true,
 
